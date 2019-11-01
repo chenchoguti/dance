ba8b2cc7bce6685d168fa97feae75acace9c3315
Kevin Gutierrez <chenchoguti.123@gmai.com>
2019-10-31 19:33:20 -0600

    se añadio la tabla de relacion de muchos a muchos la tabla show aderida a la tabla teatro

--- dance/admin.py
+++ dance/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Bailarina, Teatro
+from .models import Bailarina, BailarinaAdmin, Teatro, TeatroAdmin
 
-admin.site.register(Bailarina)
-admin.site.register(Teatro)
+admin.site.register(Bailarina, BailarinaAdmin)
+admin.site.register(Teatro, TeatroAdmin)
--- dance/models.py
+++ dance/models.py
@@ -16,25 +16,26 @@ class Teatro(models.Model):
 	historia = models.TextField()
 	fecha_creacion = models.DateTimeField(
 		default=timezone.now)
-	#bailarinas = models.ManyToManyField(Bailarina, through='Bailarina')
+	bailarinas = models.ManyToManyField(Bailarina, through='Show')
 
 	def __str__(self):
 		return self.nombre
 
 
-# class Show(models.Model):
-# 	bailarina = models.ForeignKey(Bailarina, on_delete=models.CASCADE)
+class Show(models.Model):
+	#fecha = models.DateField()
+	bailarina = models.ForeignKey(Bailarina, on_delete=models.CASCADE)
 
-# 	teatro = models.ForeignKey(Teatro, on_delete=models.CASCADE)
+	teatro = models.ForeignKey(Teatro, on_delete=models.CASCADE)
 
 
-# class ShowInLine(admin.TabularInline):
-# 	model = Show
-# 	extra = 1
+class ShowInLine(admin.TabularInline):
+	model = Show
+	extra = 1
 
-# class ActorAdmin(admin.ModelAdmin):
-# 	inlines= (ShowInLine,)
+class BailarinaAdmin(admin.ModelAdmin):
+	inlines= (ShowInLine,)
 
-# class PeliculaAdmin(admin.ModelAdmin):
-# 	inlines = (ShowInLine,)
+class TeatroAdmin(admin.ModelAdmin):
+	inlines = (ShowInLine,)
 	
